8015f9c890638cda8116892206757fa28c986001
Violape <32572554+Violape@users.noreply.github.com>
2018-11-01 08:57:12 +0800

    .aspx structure formed

---
+++ Exp5-Gridview/Gridview.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clock.Exp5_Gridview
+{
+    public partial class Gridview : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
